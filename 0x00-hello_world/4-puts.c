31ee2a46fc3fa56850a4fd031204eec7b6f24839
Test User <yosefbelachew39@gmail.com>
2022-06-02 16:47:54 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+
+
+
 /**
- *  *  * main entry point
- *   *  *
- *    *  returm
- *      **/
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
-puts("\'programming is like building a multilingual puzzle");
+                puts("\'programming is like building a multilingual puzzle");
+
 	    return (0);
 
 }
